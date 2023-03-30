a9b64d256ebda4fd9c93339e7c1d52e792f6118c
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-30 20:52:03 -0300

    Projeto 100% - sem CSS

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((ele) => (
+          <MissionCard
+            key={ ele.name }
+            name={ ele.name }
+            year={ ele.year }
+            country={ ele.country }
+            destination={ ele.destination }
+          />
+        ))}
       </div>
     );
   }
