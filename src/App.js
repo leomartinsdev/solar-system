bcb5438c3284f34d96eaa5113d73d92ffb4e2f05
Leonardo Martins <leonardogustavo@outlook.com>
2023-03-30 19:16:43 -0300

    Requisitos 1 ao cinco - commit grande porque fiz tudo em 10 minutos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    // return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
-// oi
+
 export default App;
